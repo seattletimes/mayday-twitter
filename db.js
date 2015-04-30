9e089b308a8e54eae98e79a7caffa38da66531e6
Thomas Wilburn <news@thomaswilburn.net>
2015-04-30 13:36:09 -0700

    Refactor database code into a separate module
    Much more readable this way, easier to keep track of how serialization
    works.

---
+++ db.js
@@ -0,0 +1,78 @@
+var async = require("async");
+var sqlite = require("sqlite3");
+
+var db = new sqlite.Database("tweets.db");
+
+var tableDef = {
+  name: "TEXT",
+  handle: "TEXT",
+  timestamp: "INTEGER",
+  id: "TEXT",
+  tweet: "TEXT",
+  latlng: "TEXT",
+  media: "TEXT",
+  tags: "TEXT",
+  urls: "TEXT",
+  mentions: "TEXT",
+  avatar: "TEXT"
+};
+
+var init = function(callback) {
+  db.get('SELECT * FROM sqlite_master WHERE type = "table" AND name = "tweets";', function(err, exists) {
+    if (!exists) {
+      var columns = [];
+      for (var column in tableDef) {
+        columns.push(column + " " + tableDef[column]);
+      }
+      var tweetTable = "CREATE TABLE tweets (" + columns.join(",") + ");";
+      async.parallel([
+        db.run.bind(db, tweetTable),
+        db.run.bind(db, "CREATE TABLE users (name TEXT, id TEXT);")
+      ], function(err) {
+        if (err) console.log(err);
+        callback();
+      })
+    } else callback();
+  });
+};
+
+var insertTweet = function(tweet, callback) {
+  var keys = Object.keys(tableDef);
+  var serialized = "media tags urls mentions latlng".split(" ");
+  var query = "INSERT INTO tweets (" + keys.join(",") + ") VALUES (" + keys.map(function() { return "?" }).join(",") + ")";
+  var values = [];
+  keys.forEach(function(key) {
+    var value = tweet[key];
+    values.push(serialized.indexOf(key) > -1 ? JSON.stringify(value || []) : value || "");
+  });
+  db.run(query, values, function(err) {
+    if (err) return console.log(err);
+    callback();
+  });
+};
+
+var getTweets = function(callback) {
+  db.all("SELECT * FROM tweets", function(err, result) {
+    //if we got more results, requery first
+    result = result || [];
+    result.forEach(function(tweet) {
+      ["tags", "media", "urls", "mentions", "latlng"].forEach(function(prop) {
+        tweet[prop] = JSON.parse(tweet[prop]);
+      });
+    });
+    callback(result);
+  });
+}
+
+module.exports = {
+  init: init,
+  all: db.all.bind(db),
+  get: db.get.bind(db),
+  insertUser: function(name, id, callback) {
+    db.run("INSERT INTO users (name, id) VALUES (?, ?);", [name, id], function(err) {
+      callback(err, id);
+    });
+  },
+  getTweets: getTweets,
+  insertTweet: insertTweet
+};
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var async = require("async");
 var fs = require("fs");
 var shell = require("shelljs");
-var sqlite = require("sqlite3");
 var Twitter = require("twitter");
 
 var auth = require("./auth.json");
 var distill = require("./distill");
+var db = require("./db");
 
 var client = new Twitter({
   consumer_key: auth.twitter.consumerKey,
@@ -15,22 +15,6 @@ var client = new Twitter({
 });
 
 var publish = "grunt template publish";
-
-var db = new sqlite.Database("tweets.db");
-
-var tableDef = {
-  name: "TEXT",
-  handle: "TEXT",
-  timestamp: "INTEGER",
-  id: "TEXT",
-  tweet: "TEXT",
-  latlng: "TEXT",
-  media: "TEXT",
-  tags: "TEXT",
-  urls: "TEXT",
-  mentions: "TEXT",
-  avatar: "TEXT"
-};
 
 var follow = [
   "audcarls",
@@ -58,27 +42,22 @@ var follow = [
 var dumpDB = function() {
   console.log("Dumping database to file system");
   pending = false;
-  db.all("SELECT * FROM tweets", function(err, result) {
-    //if we got more results, requery first
+  db.getTweets(function(result) {
     if (pending) {
       console.log("got more results, re-exporting");
       return dumpDB();
     }
-    result = result || [];
-    result.forEach(function(tweet) {
-      ["tags", "media", "urls", "mentions", "latlng"].forEach(function(prop) {
-        tweet[prop] = JSON.parse(tweet[prop]);
-      });
-    });
     json = JSON.stringify(result.reverse(), null, 2);
     if (!shell.test("-e", "data")) shell.mkdir("data");
     fs.writeFileSync("data/tweets.json", json);
-    inProgress = false;
     console.log("Completed file dump, building");
-    shell.exec(publish, function() {
-      console.log("Completed build");
-    });
-  });
+    // shell.exec(publish, function() {
+    //   inProgress = false;
+    //   console.log("Completed build");
+    //   //check again for anything that came in during publish
+    //   if (pending) dumpDB();
+    // });
+  })
 };
 
 
@@ -93,25 +72,7 @@ var scheduleDump = function() {
 dumpDB();
 
 async.waterfall([
-  function(callback) {
-    //init database
-    db.get('SELECT * FROM sqlite_master WHERE type = "table" AND name = "tweets";', function(err, exists) {
-      if (!exists) {
-        var columns = [];
-        for (var column in tableDef) {
-          columns.push(column + " " + tableDef[column]);
-        }
-        var tweetTable = "CREATE TABLE tweets (" + columns.join(",") + ");";
-        async.parallel([
-          db.run.bind(db, tweetTable),
-          db.run.bind(db, "CREATE TABLE users (name TEXT, id TEXT);")
-        ], function(err) {
-          if (err) console.log(err);
-          callback();
-        })
-      } else callback();
-    });
-  },
+  db.init,
   function(callback) {
     var query = "SELECT * FROM users WHERE name IN (" + follow.map(JSON.stringify).join(",") + ");";
     db.all(query, function(err, result) {
@@ -129,11 +90,9 @@ async.waterfall([
         screen_name: unidentified.join(",")
       }, function(err, response) {
         async.map(response, function(item, c) {
-          db.run("INSERT INTO users (name, id) VALUES (?, ?);", [item.screen_name, item.id_str], function(err) {
-            c(err, item.id_str);
-          });
+          db.insertUser(item.screen_name, item.id_str, c);
         }, function(err, mapped) {
-          if (err) return console.log(err);
+          if (err) return console.error(err);
           callback(null, mapped.concat(ids));
         });
       });
@@ -152,24 +111,13 @@ async.waterfall([
         if (!t || follow.indexOf(t.handle) == -1) return; //skip retweets
         if (t.tweet.indexOf("RT") == 0) return; //skip retweets
         console.log(t);
-        var keys = Object.keys(tableDef);
-        var serialized = "media tags urls mentions latlng".split(" ");
-        var query = "INSERT INTO tweets (" + keys.join(",") + ") VALUES (" + keys.map(function() { return "?" }).join(",") + ")";
-        var values = [];
-        keys.forEach(function(key) {
-          var value = t[key];
-          values.push(serialized.indexOf(key) > -1 ? JSON.stringify(value || []) : value || "");
-        });
-        db.run(query, values, function(err) {
-          if (err) return console.log(err);
-          scheduleDump();
-        });
+        db.insertTweet(t, scheduleDump);
       });
 
-      stream.on("error", console.log.bind(console));
+      stream.on("error", console.error.bind(console));
       
       stream.on("end", function(ended) {
-        console.log(ended.statusMessage);
+        console.error(ended.statusMessage);
       });
     });
   }
