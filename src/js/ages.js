1e8c60887bb00a76fa2f2a92ece1d35d41099663
Thomas Wilburn <news@thomaswilburn.net>
2016-05-01 23:17:45 -0700

    End of day

--- src/js/ages.js
+++ src/js/ages.js
@@ -1,18 +1,18 @@
 module.exports = function(timestamp) {
-  var now = new Date().getTime(); //midnight on May 1
+  var now = new Date().getTime(2016, 6, 1, 23, 59); //midnight on May 1
   var age = "recent";
   var minutes = 1000 * 60;
   var hours = 60 * minutes;
   var elapsed = now - timestamp;
-  if (elapsed > 8 * hours) {
+  if (elapsed > 16 * hours) {
     age = "ancient";
-  } else if (elapsed > 4 * hours) {
+  } else if (elapsed > 10 * hours) {
     age = "oldest";
-  } else if (elapsed > 2 * hours) {
+  } else if (elapsed > 6 * hours) {
     age = "older";
-  } else if (elapsed > 1 * hours) {
+  } else if (elapsed > 4 * hours) {
     age = "old";
-  } else if (elapsed > .5 * hours) {
+  } else if (elapsed > 2 * hours) {
     age = "young";
   }
   return age;
